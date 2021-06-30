6bac8d95333ffc83deb0b206119884ffd89890d5
jason11501 <55671823+jason11501@users.noreply.github.com>
2021-06-30 12:29:14 +0700

    Update ksyscall.h
    delete 
    SysPrintChar()

--- NachOS-4.0/code/userprog/ksyscall.h
+++ NachOS-4.0/code/userprog/ksyscall.h
@@ -68,10 +68,5 @@ void SysReadString(char *buffer, int len)
 static void WriteDone(int arg) {
   writeDone->V();
 }
-
-void SysPrintChar(char c){
-  writeDone->P() ;
-  kernel->synchConsoleOut->PutChar(c);
-}
                                       
 #endif /* ! __USERPROG_KSYSCALL_H__ */
